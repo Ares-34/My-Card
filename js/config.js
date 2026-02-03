0d9acb53ecb9f0232b882dd131fd46968d732a20
Ares <ro0oo.xn@gmail.com>
2026-02-03 19:42:50 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Site Configuration
-// IMPORTANT: Change ADMIN_ENABLED to control admin panel access
-// true  = Admin panel is enabled and accessible at admin.html
-// false = Admin panel is disabled and will redirect to main site
-
-const ADMIN_ENABLED = true;
